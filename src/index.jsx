acd10f4c6487d97382cce11307e39401a6db0ba9
zhaojinxin <386608610@qq.com>
2022-04-30 22:08:20 +0800

    feat:注册登陆功能完成

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Router from './router'
+import "antd/dist/antd.css"
+import "./assets/base.min.css"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Router />
+)
---
+++ src/request/request.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const axiosOption = {
+  baseURL:'/api',
+  timeout:5000
+}
+
+//创建一个单例
+const instance = axios.create(axiosOption);
+// 添加一个请求拦截器
+instance.interceptors.request.use(function(config){
+  return config;
+},function(error){
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+//添加响应拦截器
+instance.interceptors.response.use(function(response){
+  //添加响应拦截器
+  return response.data
+},function(error){
+  //对响应错误做点什么
+  return Promise.reject(error);
+});
+export default instance;
